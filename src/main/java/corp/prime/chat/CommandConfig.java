79a87cbefe9fc02e96c841fd59ec81e577a57e49
AssamyOfficial <ivankutcov@gmail.com>
2026-08-17 14:37:37 +0500

    Add command configuration loader

---
+++ src/main/java/corp/prime/chat/CommandConfig.java
@@ -0,0 +1,43 @@
+package corp.prime.chat;
+
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class CommandConfig {
+    private final PrimeChat plugin;
+    private YamlConfiguration config;
+
+    public CommandConfig(PrimeChat plugin) {
+        this.plugin = plugin;
+        load();
+    }
+
+    public void load() {
+        File file = new File(plugin.getDataFolder(), "command.yml");
+
+        if (!file.exists()) {
+            plugin.saveResource("command.yml", false);
+        }
+
+        config = YamlConfiguration.loadConfiguration(file);
+    }
+
+    public void reload() {
+        load();
+    }
+
+    public boolean getBoolean(String path, boolean def) {
+        return config.getBoolean(path, def);
+    }
+
+    public String getString(String path, String def) {
+        return config.getString(path, def);
+    }
+
+    public List<String> getStringList(String path) {
+        return config.getStringList(path);
+    }
+}
